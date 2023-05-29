80fc2e5176cdc68745b5b72f178e6c2762ca354a
Matheus <matheuscerqueiratb@gmail.com>
2023-05-29 19:49:06 -0300

    Criação de super classe e sobrecarga

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,5 @@
 import modelos.Filme;
+import modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -19,6 +20,18 @@ public class Principal {
 
         System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.setTemporadas(10);
+        lost.setEpisodioPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+
+        System.out.println("duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
+
+    
     }
     
 }
--- src/modelos/Filme.java
+++ src/modelos/Filme.java
@@ -1,77 +1,15 @@
 package modelos;
 
-public class Filme {
+public class Filme extends Titulo {
     
-    private String nome;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalDeAvaliacoes;
-    private int duracaoEmMinutos;
+    private String diretor;
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public void setSomaDasAvaliacoes(double somaDasAvaliacoes) {
-        this.somaDasAvaliacoes = somaDasAvaliacoes;
-    }
-
-    public void setTotalDeAvaliacoes(int totalDeAvaliacoes) {
-        this.totalDeAvaliacoes = totalDeAvaliacoes;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
-
-
-    public String getNome() {
-        return nome;
-    }
-
-    public int getAnoDeLancamento() {
-        return anoDeLancamento;
-    }
-
-    public boolean IncluidoNoPlano() {
-        return incluidoNoPlano;
-    }
-
-    public double getSomaDasAvaliacoes() {
-        return somaDasAvaliacoes;
-    }
-
-    public int getDuracaoEmMinutos() {
-        return duracaoEmMinutos;
-    }
-
-    public int getTotalDeAvaliacoes(){
-        return totalDeAvaliacoes;
-    }
-
-    public void exibeFichaTecnica() {
-        System.out.println("Nome do filme: " + nome);
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-        System.out.println("Duração do filme: " + duracaoEmMinutos);
-        System.out.println("Incluido no plano: " + incluidoNoPlano);
-    }
-
-    public void avalia(double nota) {
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes++;
-    }
-
-    public double pegaMedia() {
-        return somaDasAvaliacoes / totalDeAvaliacoes;
-    }
-
+    
 }
